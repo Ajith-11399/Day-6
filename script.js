2a6f8bf7f1013ce197642b02f250ab9ed824ddb5
Ajith D <164172785+Ajith-11399@users.noreply.github.com>
2024-03-28 17:50:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,125 @@
+//! Task - 1 => Class - Movie :-
+class Movie {
+    constructor(title, studio, rating = "PG") {
+        this.title = title;
+        this.studio = studio;
+        this.rating = rating;
+    }
+};
+const film = new Movie("Casino Royale", "EON Productions", "PG13");
+console.log(
+    "Movie Title:" + " " + film.title + ",",
+    "Studio:" + " " + film.studio + ",",
+    "Rating:" + " " + film.rating
+);
+
+
+//! Task - 1 => Class - Movie with getPG method :-
+class Movie {
+    constructor(title, studio, rating = "PG") {
+        this.title = title;
+        this.studio = studio;
+        this.rating = rating;
+    }
+    static getPG(movies) {
+        return movies.filter(movie => movie.rating === "PG")
+    }
+};
+const films = [
+    new Movie("Casino Royale", "EON Productions", "PG13"),
+    new Movie("Vikram", "RKFI", "Not Rated"),
+    new Movie("Intersteller", "Paramount Pictures", "PG-13"),
+    new Movie("Deadpool", "Marvel Studios", "PG"),
+    new Movie("Joker", "Warner.Bros", "PG"),
+    new Movie("Black Adam", "DC Films", "PG-13"),
+    new Movie("John Wick", "87Eleven Productions", "PG")
+];
+const RMovies = Movie.getPG(films).sort();
+console.log(RMovies);
+
+
+
+
+
+//! Task - 2 => Class - Circle :-
+class Circle {
+    constructor(radius, color) {
+        this.radius = radius;
+        this.color = color;
+    }
+    set Radius(n){
+        this.radius = n;
+    }
+    get Radius(){
+        return this.radius;
+    }
+    set Color(o){
+        this.color = o;
+    }
+    get Color(){
+        return this.color;
+    }
+    get toString(){
+        return `"Circle[radius = ${this.radius}, color = ${this.color}]"`
+    }
+    get Area(){
+        return Math.PI*Math.pow(this.radius,2);
+    }
+    get Circumference(){
+        return 2*Math.PI*this.radius;
+    }
+};
+const circle1 = new Circle("2.0", "Blue");
+console.log(circle1);
+console.log("Radius :" + " " + circle1.Radius);
+console.log("Color :" + " " + circle1.Color);
+console.log(circle1.toString);
+console.log(circle1.Area);
+console.log(circle1.Circumference);
+
+
+
+
+
+//! !Task - 3 => Class - Person : -
+class person {
+    constructor(name, age, dob, gender, description) {
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+        this.gender = gender;
+        this.description = description;
+    }
+};
+const Ajith = new person("AJ", "25", "11-03-1999", "Male", "Full Stack Developer");
+console.log(
+    "Name :" + " " + Ajith.name + ",",
+    "Age :" + " " + Ajith.age + ",",
+    "DOB :" + " " + Ajith.dob + ",",
+    "Gender :" + " " + Ajith.gender + ",",
+    "Job Description :" + " " + Ajith.description
+);
+
+
+
+
+
+//! Task - 4 => Class - Uber : -
+class uber {
+    constructor(price, km) {
+        this.price = price;
+        this.km = km;
+    }
+    set cost(ele){
+        this.price = ele;
+        this.km = ele;
+    }
+    // set kilometer(b){
+    //     this.km = b;
+    // }
+    get cost(){
+        return this.price*this.km;
+    }
+};
+const ride = new uber(20,10);
+console.log(ride.cost);
